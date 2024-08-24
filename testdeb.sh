340e8d6a3a8e68b9efc9e3b9f2f56d2d6933c5c5
Paulo Ricardo <paulo.rodriguespres11@gmail.com>
2024-08-24 16:36:18 -0300

    🛠️  build: Adicionando comentário ao arquivos de teste O comentário adicionado relembra que o código Python deve ser compilado, usando setup.py, antes de ser empacotado para .deb.

--- testdeb.sh
+++ testdeb.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Antes compilar código Python com:
+# python3.10 setup.py build
+
 sudo dpkg -r snakeodyssey
 sudo dpkg-deb -b build/ dist/snakeOdyssey-v$1.deb
 sudo dpkg -i dist/snakeOdyssey-v$1.deb
